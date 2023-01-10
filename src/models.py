6038b9799356e79bc5db835fe7e9531a0d30e542
Christian Lempa <christian.lempa@clcreative.de>
2023-01-10 17:15:11 +0100

    Initial commit

---
+++ src/models.py
@@ -0,0 +1,29 @@
+﻿from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    books = relationship("Book", back_populates="author")
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, unique=True)
+    description = Column(String)
+    date_published = Column(DateTime)
+    date_updated = Column(DateTime)
+    author_id = Column(Integer, ForeignKey("users.id"))
+
+    author = relationship("User", back_populates="books")
---
+++ src/schemas.py
@@ -0,0 +1,36 @@
+﻿from pydantic import BaseModel
+
+
+class BookBase(BaseModel):
+    title: str
+    description: str | None = None
+
+
+class BookCreate(BookBase):
+    pass
+
+
+class Book(BookBase):
+    id: int
+    author_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    books: list[Book] = []
+
+    class Config:
+        orm_mode = True
