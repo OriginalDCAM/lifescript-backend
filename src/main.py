9d87611e275ecec29cd31fd485008d1f2b4e2cc9
Christian Lempa <christian.lempa@clcreative.de>
2023-01-10 17:19:29 +0100

    Revert "Revert "Initial commit""
    This reverts commit a9e5340410a20c304bd50f33749d92951a20fdc8.

---
+++ src/main.py
@@ -0,0 +1,18 @@
+﻿from fastapi import FastAPI
+from .routers import books, pages, users
+
+app = FastAPI()
+
+app.include_router(books.router)
+app.include_router(pages.router)
+app.include_router(users.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Test"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
---
+++ src/routers/users.py
@@ -0,0 +1,23 @@
+﻿from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get('/login/', tags=['users'])
+async def login():
+    return 'login'
+
+
+@router.get('/register/', tags=["users"])
+async def register():
+    return 'register'
+
+
+@router.post('login', tags=['users'])
+async def login():
+    return 'test'
+
+
+@router.post('register', tags=['users'])
+async def register():
+    return 'test'
