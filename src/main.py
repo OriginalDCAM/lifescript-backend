e407721b4409fe3cba72eeed05551af91b5dc0de
OriginalDCAM <denzelannette@hotmail.com>
2023-01-11 23:04:56 +0100

    Renamed routers file to routes

--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
-﻿from fastapi import FastAPI
+﻿from fastapi import FastAPI, Depends, HTTPException
 from .routers import books, pages, users
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(books.router)
 app.include_router(pages.router)
 app.include_router(users.router)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
@@ -16,3 +30,9 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/test/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
---
+++ src/routes/books.py
@@ -0,0 +1,32 @@
+﻿from fastapi import FastAPI, Depends, HTTPException, APIRouter
+from sqlalchemy.orm import Session
+
+router = APIRouter(
+    prefix='/books',
+    tags=['books']
+)
+
+
+@router.get('/')
+async def get_all_books():
+    return 'All Books'
+
+
+@router.get('{name}')
+async def get_book():
+    return 'Book'
+
+
+@router.post('/create')
+async def create_book():
+    pass
+
+
+@router.delete('/{id}')
+async def delete_book():
+    pass
+
+
+@router.put('/{id}')
+async def update_book():
+    pass
