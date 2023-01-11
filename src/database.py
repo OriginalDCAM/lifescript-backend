4a0cdccfbcbd6e2c330aa9ff24c6e6d83f56f06f
OriginalDCAM <denzelannette@hotmail.com>
2023-01-11 15:04:56 +0100

    updated connection string

--- src/database.py
+++ src/database.py
@@ -2,7 +2,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:123@db/LifeScript"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
