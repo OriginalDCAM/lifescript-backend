524ea4d610504a273f909438cdd798eac6a89865
OriginalDCAM <denzelannette@hotmail.com>
2023-01-14 12:58:54 +0100

    Added Alembic to the project

---
+++ alembic/versions/2023_01_13_2047-922d4c30a30f_test.py
@@ -0,0 +1,24 @@
+"""Test
+
+Revision ID: 922d4c30a30f
+Revises: 4750c2d794ad
+Create Date: 2023-01-13 20:47:56.076802
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '922d4c30a30f'
+down_revision = '4750c2d794ad'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ alembic/versions/2023_01_13_2050-a49a3226c3a0_test_2.py
@@ -0,0 +1,24 @@
+"""Test 2
+
+Revision ID: a49a3226c3a0
+Revises: 922d4c30a30f
+Create Date: 2023-01-13 20:50:02.199083
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a49a3226c3a0'
+down_revision = '922d4c30a30f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
