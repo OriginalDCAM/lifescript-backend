846111fbdc278ee3a967389272361dc2e666d1cc
OriginalDCAM <denzelannette@hotmail.com>
2023-01-14 14:02:49 +0100

    Renamed the APIRouter back to router

--- src/books/router.py
+++ src/books/router.py
@@ -1,52 +1,52 @@
 ﻿from fastapi import FastAPI, Depends, HTTPException, APIRouter
 from sqlalchemy.orm import Session
 
-books = APIRouter(
+router = APIRouter(
     prefix='/books',
     tags=['books']
 )
 
 
-@books.get('/')
+@router.get('/')
 async def get_all_books():
     return 'All Books'
 
 
-@books.get('{name}')
+@router.get('{name}')
 async def get_book():
     return 'Book'
 
 
-@books.post('/create')
+@router.post('/create')
 async def create_book():
     pass
 
 
-@books.delete('/{id}')
+@router.delete('/{id}')
 async def delete_book():
     pass
 
 
-@books.put('/{id}')
+@router.put('/{id}')
 async def update_book():
     pass
 
 
-@books.get('/page/{id}')
+@router.get('/page/{id}')
 async def get_book_page():
     return 'This is awesome'
 
 
-@books.post('/create')
+@router.post('/create')
 async def create_book_page():
     pass
 
 
-@books.delete('/{id}')
+@router.delete('/{id}')
 async def delete_book_page():
     pass
 
 
-@books.put('/{id}')
+@router.put('/{id}')
 async def update_book_page():
     pass
--- src/users/router.py
+++ src/users/router.py
@@ -5,33 +5,33 @@ from src.users import crud, schemas
 
 from src.database import get_db
 
-users = APIRouter(
+router = APIRouter(
     prefix='/api/v1/users',
     tags=['users']
 )
 
 
-@users.get('/login')
+@router.get('/login')
 async def login():
     return 'login'
 
 
-@users.get('/register')
+@router.get('/register')
 async def register():
     return 'register'
 
 
-@users.post('/login')
+@router.post('/login')
 async def login():
     return 'test'
 
 
-@users.post('/register')
+@router.post('/register')
 async def register():
     return 'test'
 
 
-@users.get("/", response_model=list[schemas.User])
+@router.get("/", response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
