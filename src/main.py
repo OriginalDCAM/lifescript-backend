902c0863da8ecb856f97885d51ecbb3584a35146
OriginalDCAM <denzelannette@hotmail.com>
2023-01-14 14:03:18 +0100

    Updated the imports

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 ﻿from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException
-from src.books.router import books
-from src.users.router import users
+from src.books.router import router as books_router
+from src.users.router import router as users_router
 
 app = FastAPI()
 
-app.include_router(books)
-app.include_router(users)
+app.include_router(books_router)
+app.include_router(users_router)
