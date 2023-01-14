a02f816ccca6e0256d267f0211d673d2f19cf5d9
OriginalDCAM <denzelannette@hotmail.com>
2023-01-14 13:05:50 +0100

    Redesigned main file

--- src/main.py
+++ src/main.py
@@ -1,20 +1,9 @@
-﻿from fastapi import FastAPI, Depends, HTTPException
-
-from sqlalchemy.orm import Session
-
-from src.routes import books, pages, users
-from src import schemas, models, crud
-from src.database import get_db
-
+﻿from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends, HTTPException
+from src.books.router import books
+from src.users.router import users
 
 app = FastAPI()
 
-app.include_router(books.router)
-app.include_router(pages.router)
-app.include_router(users.router)
-
-
-@app.get("/test/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
+app.include_router(books)
+app.include_router(users)
