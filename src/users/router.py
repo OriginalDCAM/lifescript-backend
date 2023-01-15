6a3fc2cb94016fe1e5ec33d2770ab7b111b2d1ad
OriginalDCAM <denzelannette@hotmail.com>
2023-01-15 19:27:09 +0100

    added new routes

--- src/users/router.py
+++ src/users/router.py
@@ -1,4 +1,4 @@
-﻿from fastapi import APIRouter, Depends
+﻿from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from src.users import crud, schemas
@@ -11,27 +11,22 @@ router = APIRouter(
 )
 
 
-@router.get('/login')
-async def login():
-    return 'login'
-
-
-@router.get('/register')
-async def register():
-    return 'register'
-
-
-@router.post('/login')
-async def login():
-    return 'test'
-
-
-@router.post('/register')
-async def register():
-    return 'test'
-
-
 @router.get("/", response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
+
+
+@router.post("/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@router.get("/{user_id}", response_model=schemas.User)
+def reader_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
