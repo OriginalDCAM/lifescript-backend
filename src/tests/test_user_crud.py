c43266353bb38fa5e69b4a1b0a8915c098f02bfa
OriginalDCAM <denzelannette@hotmail.com>
2023-01-30 22:48:24 +0100

    Added a test for users

--- src/tests/test_user_crud.py
+++ src/tests/test_user_crud.py
@@ -1,10 +1,47 @@
+import os
+import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
 from src.main import app
+from src.database import Base, get_db
+
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_TEST_HOST')}/{os.getenv('POSTGRES_TEST_DB')}"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def test_db():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
 
 def test_users_read():
-    response = client.get("/api/v1/users/?skip=0&limit=100")
+    response = client.post("/api/v1/users/",
+                           json={"first_name": "Test", "username": "TestUser", "email": "testuser@test.com", "password": "thisisatestpassword"},)
     assert response.status_code == 200
