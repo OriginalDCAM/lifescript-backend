2c699dd95cb7e25bdf9641d38849e1d6979d39c0
OriginalDCAM <denzelannette@hotmail.com>
2023-04-16 14:13:45 +0200

    Implemented a JWT access token generator in hashing.py

--- src/helpers/hashing.py
+++ src/helpers/hashing.py
@@ -1,4 +1,15 @@
+import os
+import datetime
+
 from passlib.context import CryptContext
+from typing import Union, Any
+from jose import jwt
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7
+ALGORITHM = "HS256"
+JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+JWT_REFRESH_SECRET_KEY = os.getenv('JWT_REFRESH_SECRET_KEY')
 
 pwd_content = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,5 +20,17 @@ class Hasher():
         return pwd_content.verify(plain_password, hashed_password)
 
     @staticmethod
-    def get_password_hash(password):
+    def get_password_hash(password: Union[str, Any]) -> str:
         return pwd_content.hash(password)
+
+    @staticmethod
+    def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+        if expires_delta is not None:
+            expires_delta = datetime.utcnow() + expires_delta
+        else:
+            expires_delta = datetime.utcnow(
+            ) + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+        to_encode = {"exp": expires_delta, "sub": str(subject)}
+        encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
+        return encoded_jwt
