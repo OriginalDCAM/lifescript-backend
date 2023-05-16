af9b0e829e14505e97daaea85bbccca7288785ee
OriginalDCAM <denzelannette@hotmail.com>
2023-05-16 23:23:54 +0200

    Added more to the config of openapi

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 ﻿from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.utils import get_openapi
 
 from src.books.router import router as books_router
 from src.users.router import router as users_router
+from src.pages.router import router as page_router
 
 app = FastAPI()
 
@@ -18,6 +20,24 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="LifeScript API",
+        version="1.0.0",
+        description="API for LifeScript",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
 
 app.include_router(books_router)
 app.include_router(users_router)
+app.include_router(page_router)
+
+app.openapi = custom_openapi
