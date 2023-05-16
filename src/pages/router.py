e265b0c0bfedf261861980f2a59bc7ebe0e9ba80
OriginalDCAM <denzelannette@hotmail.com>
2023-05-16 23:24:31 +0200

    Added api routes for so frontend can fetch these

--- src/pages/router.py
+++ src/pages/router.py
@@ -2,21 +2,20 @@
 from sqlalchemy.orm import Session
 
 from src.books import crud, schemas
+from src.pages import crud, schemas
 
 from src.database import get_db
 
 router = APIRouter(
-    prefix='/api/v1/books',
-    tags=['books']
+    prefix='/api/v1/pages',
+    tags=['pages']
 )
 
 
-@router.post("/create_book/{user_id}", response_model=schemas.Book)
-def create_book_for_user(user_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)):
-    return crud.create_user_book(db=db, book=book, user_id=user_id)
 
-
-@router.get("/", response_model=list[schemas.Book])
-def read_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    books = crud.get_books(db, skip=skip, limit=limit)
-    return books
+@router.get("/{page_id}", response_model=schemas.Page)
+def read_page(page_id: int, db: Session = Depends(get_db)):
+    page = crud.get_page(db, page_id=page_id)
+    if page is None:
+        raise HTTPException(status_code=404, detail="Page not found")
+    return page
